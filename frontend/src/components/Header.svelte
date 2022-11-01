33ca72ff32100c5378a9c3b6f931d8036ee7b889
ahehiohyou <diman_vladimirov34@mail.ru>
2022-11-01 21:42:45 +0300

    added sass and basic page layout with components

---
+++ frontend/src/components/Header.svelte
@@ -0,0 +1,48 @@
+<svelte:head>
+	<title>Wellcum!</title>
+</svelte:head>
+
+<header class="global-header">
+	<nav class="navbar">
+		<div class="navbar__item title">My Best App</div>
+		<a class="navbar__item" href="/">Home</a>
+		<a class="navbar__item" href="/about">About</a>
+		<a class="navbar__item" href="/cat-facts">Cat Facts</a>
+		<a class="navbar__item" href="/test">Test</a>
+		<a class="navbar__item" href="/login">Login</a>
+	</nav>
+</header>
+
+<style lang="scss">
+	.global-header {
+		background-color: #313131;
+		height: 56px;
+		color: white;
+		padding: 15px;
+		box-sizing: border-box;
+
+		.navbar {
+			display: flex;
+			align-items: center;
+			height: 100%;
+
+			.logo {
+				height: 100%;
+				margin-right: 5px;
+			}
+
+			.title {
+				font-weight: bold;
+			}
+
+			.navbar__item {
+				text-decoration: none;
+				color: white;
+			}
+
+			.navbar__item:not(:last-child) {
+				margin-right: 30px;
+			}
+		}
+	}
+</style>
---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import Header from '@/components/Header.svelte';
+	import Footer from '@/components/Footer.svelte';
+</script>
+
+<Header />
+<slot />
+<Footer />
+
+<style lang="scss">
+	:root {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+			'Open Sans', 'Helvetica Neue', sans-serif;
+		height: 100%;
+	}
+
+	:global(body) {
+		margin: 0;
+		height: 100%;
+		background-color: #fbfbfb;
+	}
+
+	:global(.title) {
+		position: relative;
+	}
+
+	:global(.title__text::before) {
+		content: '';
+		position: absolute;
+		height: 2px;
+		width: 100%;
+		background-color: rgb(139, 139, 139);
+		bottom: -5px;
+		z-index: 1;
+	}
+</style>
