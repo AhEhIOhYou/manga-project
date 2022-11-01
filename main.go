c9eb1d6f40db8c6e61a1161721d09259e3e36168
ahehiohyou <diman_vladimirov34@mail.ru>
2022-11-01 19:49:19 +0300

    add gin and serve "/" on 8095 port

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 func main()  {
-	println("hello, ahehiohyou!");
+	router := gin.Default()
+	router.Any("", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"status": "success"});
+	});
+	log.Fatal(router.Run(":8095"));
 }
