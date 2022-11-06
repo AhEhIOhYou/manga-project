cb4b24748b149358391c388563e28bcab431104e
ahehiohyou <diman_vladimirov34@mail.ru>
2022-11-06 22:09:08 +0300

    fucking carousel edited

--- client/src/components/Carousel.svelte
+++ client/src/components/Carousel.svelte
@@ -6,7 +6,7 @@
 	export let items: Array<any> = [...Array(10).keys()];
 	let scrollBy: number = 1;
 
-	const paginationFactor: number = 175;
+	const paginationFactor: number = 170;
 	const totalPaginationPixels: number = scrollBy * paginationFactor;
 
 	let active: number = 1;
@@ -23,7 +23,7 @@
 			active--;
 			offset += totalPaginationPixels;
 		} else if (atEnd) {
-			offset = 175;
+			offset = 170;
 			active = 0;
 		} else if (atStart) {
 			offset = paginationFactor * (items.length - 1 - scrollBy) * -1;
@@ -32,69 +32,136 @@
 	};
 </script>
 
-<div class="book-carousel">
-	<div class="items" style="transform: translateX({offset}px);">
-		{#each items as item, i}
-			<a
-				href={item.url}
-				class="item"
-				class:active={active === i}
-				style="background-image: url({item.cover}), linear-gradient(45deg, #6e9e2f, #d6f3af);"
-			>
-				<div class="item__info">
-					<div class="item__info__chapter">
-						{item.chapter}
-					</div>
-					<div class="item__info__title">
-						{clipText(item.title)}
-					</div>
-				</div>
-			</a>
-		{/each}
+<div class="carousel-container">
+	<div class="nav">
+		<button class="left" on:click={() => move(-1)} />
 	</div>
-	<button on:click={() => move(-1)}>&lsaquo; Prev</button>
-	<button on:click={() => move(1)}>Next &rsaquo;</button>
+	<div class="carousel">
+		<div class="items" style="transform: translateX({offset}px);">
+			{#each items as item, i}
+				<a
+					href={item.url}
+					class="item"
+					class:active={active === i}
+					style="background-image: url({item.cover}), linear-gradient(45deg, #6e9e2f, #d6f3af);"
+				>
+					<div class="item__info">
+						<div class="item__info__chapter">
+							{item.chapter}
+						</div>
+						<div class="item__info__title">
+							{clipText(item.title)}
+						</div>
+					</div>
+				</a>
+			{/each}
+		</div>
+	</div>
+	<div class="nav">
+		<button class="right" on:click={() => move(1)} />
+	</div>
 </div>
 
 <style lang="scss">
-	.book-carousel {
-		width: 525px;
-		overflow: hidden;
+	.carousel-container {
+		display: flex;
+		justify-content: center;
+		width: 620px;
 
-		.items {
+		@media screen and (max-width: 620px) {
+				width: 480px;
+		}
+
+		.nav {
 			display: flex;
-			transition: transform 0.4s ease-in-out;
-			transform: translateX(0px);
+			flex-direction: column;
+			justify-content: center;
+		}
 
-			.item {
-				min-width: 170px;
-				height: 250px;
-				margin: 0 4px;
-				border-radius: 0.7rem;
+		button {
+			display: inline-block;
+			width: 4em;
+			height: 4em;
+			border: none;
+			background: #36c148;
+			border-radius: 50%;
+
+			&:hover {
+				background-color: #d6f3af;
+				cursor: pointer;
+			}
+
+			&.left:after {
+				content: '';
+				display: inline-block;
+				margin-top: 0.4em;
+				margin-left: 0.6em;
+				width: 1.4em;
+				height: 1.4em;
+				border-top: 0.2em solid white;
+				border-right: 0.2em solid white;
+				-moz-transform: rotate(-135deg);
+				-webkit-transform: rotate(-135deg);
+				transform: rotate(-135deg);
+			}
+
+			&.right:after {
+				content: '';
+				display: inline-block;
+				margin-top: 0.4em;
+				margin-left: -0.6em;
+				width: 1.4em;
+				height: 1.4em;
+				border-top: 0.2em solid white;
+				border-right: 0.2em solid white;
+				-moz-transform: rotate(45deg);
+				-webkit-transform: rotate(45deg);
+				transform: rotate(45deg);
+			}
+		}
+
+		.carousel {
+			width: 510px;
+			overflow: hidden;
+
+			&::-webkit-scrollbar {
+				display: none;
+			}
+
+			.items {
 				display: flex;
-				justify-content: end;
-				flex-direction: column;
-				color: white;
-				transform: scale(0.8);
-				transition: transform ease 0.3s;
-				overflow: hidden;
-				&.active {
-					transform: scale(1);
-				}
+				transition: transform 0.4s ease-in-out;
+				transform: translateX(0px);
 
-				.item__info {
-					max-height: 100px;
-					min-height: 50px;
+				.item {
+					min-width: 170px;
+					height: 250px;
+					// margin: 0 4px;
+					border-radius: 0.7rem;
 					display: flex;
+					justify-content: end;
 					flex-direction: column;
-					justify-content: space-around;
-					padding: 10px;
-					background: linear-gradient(0deg, black, 85%, rgba(255, 252, 252, 0));
-				}
+					color: white;
+					transform: scale(0.8);
+					transition: transform ease 0.3s;
+					overflow: hidden;
+					&.active {
+						transform: scale(1);
+					}
 
-				.item__info__title {
-					padding-top: 5px;
-					text-overflow: clip;
+					.item__info {
+						max-height: 100px;
+						min-height: 50px;
+						display: flex;
+						flex-direction: column;
+						justify-content: space-around;
+						padding: 10px;
+						background: linear-gradient(0deg, black, 85%, rgba(255, 255, 255, 0));
+					}
+
+					.item__info__title {
+						padding-top: 5px;
+					}
 				}
 			}
 		}
