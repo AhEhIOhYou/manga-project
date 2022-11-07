c61e87222283e7b64b96b327eae5f2cd8cfc7f4d
ahehiohyou <diman_vladimirov34@mail.ru>
2022-11-07 15:20:03 +0300

    new util added

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,2 +1,17 @@
 export const clipText = (text: string, maxlength: number = 45): string =>
 	text.length > maxlength ? text.slice(0, maxlength) + '...' : text;
+export const detectMobile = (userAgent: string): boolean => {
+	const toMatch = [
+		/Android/i,
+		/webOS/i,
+		/iPhone/i,
+		/iPad/i,
+		/iPod/i,
+		/BlackBerry/i,
+		/Windows Phone/i
+	];
+
+	return toMatch.some((toMatchItem) => {
+		return userAgent.match(toMatchItem);
+	})
+}
