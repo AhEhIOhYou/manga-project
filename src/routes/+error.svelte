3c1443d301b5366fd5692313fe4343194f4eb730
ahehiohyou <diman_vladimirov34@mail.ru>
2022-11-14 12:25:14 +0300

    styles updated, added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<h1 class="page-error p-absolute">{$page.status}: {$page.error?.message}</h1>
+
+<style lang="scss">
+	.page-error {
+		top: 50%;
+		left: 50%;
+		margin-right: -50%;
+		transform: translate(-50%, -50%);
+	}
+</style>
