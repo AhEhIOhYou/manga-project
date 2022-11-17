f8c72b0c4c3d2d3bcfd457ba2473838f192a816e
ahehiohyou <diman_vladimirov34@mail.ru>
2022-11-17 23:29:27 +0300

    formating

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import Header from '@/lib/components/Header.svelte';
 	import Footer from '@/lib/components/Footer.svelte';
 
-	import '../styles/styles.scss'
+	import '../styles/styles.scss';
 </script>
 
 <Header />
