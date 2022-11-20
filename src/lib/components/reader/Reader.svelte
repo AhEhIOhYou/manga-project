24c14744401e3c027bd4ee98a0c76476326784d9
ahehiohyou <diman_vladimirov34@mail.ru>
2022-11-21 00:20:51 +0300

    edited reader page

--- src/lib/components/reader/Reader.svelte
+++ src/lib/components/reader/Reader.svelte
@@ -4,6 +4,39 @@
 </script>
 
 <div class="container">
+	<div class="reader-menu dp-flex jsc-space-between mtb-10">
+		<div class="select-view">
+			<div class="select-chapter dp-i-block">
+				<div class="dp-i-block">
+					<a class="btn" href="#">
+						Select chapter
+					</a>
+				</div>
+			</div>
+			<div class="select-pagestyle dp-i-block">
+				<select>
+					<option>
+						List style
+					</option>
+					<option>
+						Page style
+					</option>
+				</select>
+			</div>
+		</div>
+		<div class="select-pagination">
+			<div class="nav-prev dp-i-block">
+				<a class="btn prev_page" href="#">
+					Prev
+				</a>
+			</div>
+			<div class="nav-next dp-i-block">
+				<a class="btn next_page" href="#">
+					Next
+				</a>
+			</div>
+		</div>
+	</div>
 	<div class="reader-body w-100 h-100 p-relative">
 		<div class="image-list m-auto">
 			<div class="image-item p-relative">
@@ -23,9 +56,51 @@
 			</div>
 		</div>
 	</div>
+	<div class="reader-menu dp-flex jsc-space-between mtb-10">
+		<div class="select-view">
+			<div class="select-chapter dp-i-block">
+				<div class="dp-i-block">
+					<a class="btn" href="#">
+						Select chapter
+					</a>
+				</div>
+			</div>
+			<div class="select-pagestyle dp-i-block">
+				<select>
+					<option>
+						List style
+					</option>
+					<option>
+						Page style
+					</option>
+				</select>
+			</div>
+		</div>
+		<div class="select-pagination">
+			<div class="nav-prev dp-i-block">
+				<a class="btn prev_page" href="#">
+					Prev
+				</a>
+			</div>
+			<div class="nav-next dp-i-block">
+				<a class="btn next_page" href="#">
+					Next
+				</a>
+			</div>
+		</div>
+	</div>
 </div>
 
 <style lang="scss">
+	.reader-menu {
+		.select-pagination {
+			.nav-prev {
+				.prev_page {
+					
+				}
+			}
+		}
+	}
 	.reader-body {
 		.image-list {
 			overflow: hidden;
--- src/routes/[name=string]/[chapter=chapter]/+page.svelte
+++ src/routes/[name=string]/[chapter=chapter]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import Reader from '@/lib/components/reader/Reader.svelte';
+	import Comments from '@/lib/components/comment/Comments.svelte';
 
 	export let data: PageData;
 </script>
@@ -10,7 +11,7 @@
 </svelte:head>
 
 <Reader bookId={1} chapterId={1}/>
-
+<Comments bookId={1} chapterId={1}/>
 
 <style lang="scss">
 	
