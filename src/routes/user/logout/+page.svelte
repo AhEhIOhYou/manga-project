c1bf613c4765020b3651d5c9001770e5b13ca2a2
ahehiohyou <diman_vladimirov34@mail.ru>
2022-12-28 14:14:21 +0300

    added user logout endpoint

---
+++ src/routes/user/logout/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		const res = await fetch('/api/method/user.logout');
+		if (res.ok) {
+			location.href = '/';
+		}
+	});
+</script>
