3406676568bc18c49bb9143f860781150f634c2b
ahehiohyou <diman_vladimirov34@mail.ru>
2022-12-28 14:19:29 +0300

    added animation

--- src/routes/[name=string]/+page.svelte
+++ src/routes/[name=string]/+page.svelte
@@ -1,12 +1,37 @@
 <script lang="ts">
 	import type { PageServerData } from './$types';
 	// import Comments from "@/lib/components/comment/Comments.svelte";
-	import Book from "@/lib/components/Book.svelte";
+	import Book from '@/lib/components/Book.svelte';
 	import ChaptersList from '@/lib/components/chapter/ChaptersList.svelte';
+	import { onMount } from 'svelte';
+	import { fly } from 'svelte/transition';
+	import { cubicOut } from 'svelte/easing';
 
 	export let data: PageServerData;
+
+	let ready = false;
+	onMount(() => (ready = true));
 </script>
 
-<Book bookData={data.bookInfo} />
-<ChaptersList chaptersData={data.chaptersInfo} showAll={false}/>
-<!-- <Comments bookId={1} /> -->
+{#if ready}
+	<div
+		in:fly={{
+			y: 30,
+			delay: 70 * 1,
+			easing: cubicOut
+		}}
+	>
+		<Book bookData={data.bookInfo} />
+	</div>
+
+	<div
+	in:fly={{
+		y: 30,
+		delay: 70 * 2,
+		easing: cubicOut
+	}}
+>
+	<ChaptersList chaptersData={data.chaptersInfo} showAll={false} />
+</div>
+	<!-- <Comments bookId={1} /> -->
+{/if}
