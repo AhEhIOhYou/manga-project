86f0026010de3dc488ed786da34d80f9669cab39
ahehiohyou <diman_vladimirov34@mail.ru>
2022-12-28 14:08:33 +0300

    cleare hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,17 +1,5 @@
 import type { Handle } from '@sveltejs/kit';
-import { VITE_JWT_KEY } from '$env/static/private';
-import * as jwt from 'jsonwebtoken';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	// const token = event.cookies.get('token');
-	// if (token) {
-	// 	const userData = jwt.verify(token, VITE_JWT_KEY) as Record<any, any>;
-	// 	if (userData) {
-	// 		event.locals.user = {
-	// 			name: userData.username,
-	// 		};
-	// 	}
-	// }
-	
 	return resolve(event);
 };
