f4596a57271210b086acc80eb025f40574fd564d
ahehiohyou <diman_vladimirov34@mail.ru>
2023-05-26 14:16:27 +0300

    update showcase tile

--- src/lib/components/ShowcaseTile.svelte
+++ src/lib/components/ShowcaseTile.svelte
@@ -16,67 +16,46 @@
 {#if ready}
 	<div class="tile-list">
 		{#each items as item, i}
-			<div
-				in:fade={{
-					delay: 100 * i,
-					easing: quartIn
-				}}
-				class="tile-list__item"
-			>
+			<div class="tile-item" in:fade={{ delay: 100 * i, easing: quartIn }}>
 				<a href={item.link_title}>
 					<img src={item.cover} alt="img" />
-					<div class="tile-list__item__text">{item.title}</div>
+					<div class="title">{item.title}</div>
 				</a>
 			</div>
 		{/each}
 	</div>
 	{#if showEmpty}
-		<div
-			class="info"
-			in:fade={{
-				delay: 100,
-				easing: quartIn
-			}}
-		>
-			Nothing was found
-		</div>
+		<div class="info" in:fade={{ delay: 100, easing: quartIn }}>Nothing was found</div>
 	{/if}
 {/if}
 
 <style lang="scss">
 	.tile-list {
 		display: grid;
-		grid-template-columns: repeat(5, 1fr);
-		gap: 5px;
-		margin-top: 20px;
+		grid-gap: 5px;
+		grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+		grid-auto-rows: 330px;
 
-		@media (max-width: 1124px) {
-			grid-template-columns: repeat(4, 1fr);
-		}
-
-		@media (max-width: 768px) {
-			grid-template-columns: repeat(2, 1fr);
-		}
-
-		@media (max-width: 480px) {
-			grid-template-columns: 1fr;
-		}
-
-		.tile-list__item {
-			background-color: var(--white);
-			border-radius: 3px;
-			box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+		.tile-item {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			font-size: 2em;
+			color: #20263f;
 			overflow: hidden;
 			transition: all 0.2s ease-in-out;
-			position: relative;
 
-			&:hover {
-				transform: translateY(-5px);
+			img {
+				width: 250px;
+				height: 330px;
+				object-fit: cover;
+				border-radius: 3px;
 			}
 
-			&__text {
+			.title {
 				position: absolute;
 				bottom: 0;
+				margin: 0;
 				padding: 10px 10px;
 				max-height: 50%;
 				width: 100%;
@@ -87,16 +66,21 @@
 				background: linear-gradient(0deg, rgba(0, 0, 0, 0.8) 40%, rgba(0, 0, 0, 0) 100%);
 				text-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
 				word-break: break-word;
+				border-radius: 3px;
+
+				@media (max-width: 1200px) {
+					opacity: 1;
+				}
 			}
 
-			&:hover .tile-list__item__text {
-				opacity: 1;
-			}
+			@media (min-width: 1200px) {
+				&:hover .title {
+					opacity: 1;
+				}
 
-			img {
-				width: 100%;
-				height: 100%;
-				object-fit: cover;
+				&:hover {
+					transform: translateY(-5px);
+				}
 			}
 		}
 	}
